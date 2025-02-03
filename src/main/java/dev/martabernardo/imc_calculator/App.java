ebd5670bf12e4086323b4dd47d2501c22a1c81cf
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-02-03 20:06:39 +0100

    Initial commit

---
+++ src/main/java/dev/martabernardo/imc_calculator/App.java
@@ -0,0 +1,9 @@
+package dev.martabernardo.imc_calculator;
+
+public final class App {
+    private App() {
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/dev/martabernardo/imc_calculator/AppTest.java
@@ -0,0 +1,14 @@
+package dev.martabernardo.imc_calculator;
+
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.DisplayName;
+
+
+class AppTest {
+    @Test 
+    @DisplayName("Test app functionality") 
+    void testMain() { 
+        App.main(new String[]{}); 
+    }
+}
