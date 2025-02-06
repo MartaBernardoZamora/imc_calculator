dc6e98bdc35dcfe1a306ea6e4b10fd7e4a99a6a1
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-02-06 21:23:47 +0100

    Status ok Test printResultMessage

--- src/main/java/dev/martabernardo/imc_calculator/views/ResultView.java
+++ src/main/java/dev/martabernardo/imc_calculator/views/ResultView.java
@@ -1,9 +1,19 @@
 package dev.martabernardo.imc_calculator.views;
 
 public class ResultView {
+    private String getIMCCategory(float imc) {
+        if (imc < 16) return "Delgadez severa";
+        if (imc < 17) return "Delgadez moderada";
+        if (imc < 18.5) return "Delgadez leve";
+        if (imc < 25) return "Peso normal";
+        if (imc < 30) return "Sobrepeso";
+        if (imc < 35) return "Obesidad leve";
+        if (imc < 40) return "Obesidad moderada";
+        return "Obesidad mórbida";
+    }
 
     public void printResultMessage(Float imc) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'printResultMessage'");
+        System.out.println("Su índice de masa corporal es: " + imc);
+        System.out.println("Usted presenta: " + getIMCCategory(imc));
     }
 }
