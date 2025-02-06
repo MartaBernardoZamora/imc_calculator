5c057b47179c8ba7ccfdd22155bd1b0b6716816a
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-02-06 21:54:43 +0100

    Test RequestIMC  created and failed

--- src/main/java/dev/martabernardo/imc_calculator/controllers/Controller.java
+++ src/main/java/dev/martabernardo/imc_calculator/controllers/Controller.java
@@ -1,13 +1,5 @@
 package dev.martabernardo.imc_calculator.controllers;
 
-import dev.martabernardo.imc_calculator.models.Person;
-
 public class Controller {
-
-    public Float requestIMC(Float height, Float weight) {
-        Person person = new Person(weight, height);
-        Float imc = person.calculateIMC();
-        return imc;
-    }
 
 }
--- src/test/java/dev/martabernardo/imc_calculator/controllers/ControllerTest.java
+++ src/test/java/dev/martabernardo/imc_calculator/controllers/ControllerTest.java
@@ -2,19 +2,43 @@ package dev.martabernardo.imc_calculator.controllers;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import dev.martabernardo.imc_calculator.views.ResultView;
 
 public class ControllerTest {
 
     @Test
     @DisplayName("Test receiving height and weight")
     void testGetUserData() {
-        Float height = (float)1.70;
-        Float weight = (float)70.5;
-        Controller controller = new Controller();
-        Float result = controller.requestIMC(height, weight);
-        assertThat(result, is((float) 24.39));
+        ResultView spyResultView = spy(ResultView.class);
+
+        Float height = 1.70f;
+        Float weight = 70.0f;
+        Controller controller = new Controller(spyResultView);
+
+        controller.requestIMC(height, weight);
+
+        assertThat(height, is(1.70f));
+        assertThat(weight, is(70.0f));
+    }
+    @Test
+    @DisplayName("Test requestIMC method")
+    void testRequestIMC() {
+        ResultView spyResultView = spy(ResultView.class);
+        Controller controller = new Controller(spyResultView);
+        
+        Float height = 1.70f;
+        Float weight = 70.5f;
+        controller.requestIMC(height, weight);
+        Float imc= 24.39f;
+
+        verify(spyResultView, times(1)).printResultMessage(imc);
     }
 }
