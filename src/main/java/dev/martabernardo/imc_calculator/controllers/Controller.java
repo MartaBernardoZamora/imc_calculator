61483aa6a4d82a474b295933a7e753a076f468e8
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-02-06 21:57:08 +0100

    Status ok Test RequestIMC

--- src/main/java/dev/martabernardo/imc_calculator/controllers/Controller.java
+++ src/main/java/dev/martabernardo/imc_calculator/controllers/Controller.java
@@ -1,5 +1,20 @@
 package dev.martabernardo.imc_calculator.controllers;
 
+import dev.martabernardo.imc_calculator.models.Person;
+import dev.martabernardo.imc_calculator.views.ResultView;
+
 public class Controller {
+
+    private ResultView resultView;
+    
+    public Controller(ResultView resultView) {
+        this.resultView = resultView;
+    }
+    
+    public void requestIMC(Float height, Float weight) {
+        Person person = new Person(weight, height);
+        Float imc = person.calculateIMC();
+        resultView.printResultMessage(imc);
+    }
 
 }
--- src/test/java/dev/martabernardo/imc_calculator/controllers/ControllerTest.java
+++ src/test/java/dev/martabernardo/imc_calculator/controllers/ControllerTest.java
@@ -15,20 +15,6 @@ import dev.martabernardo.imc_calculator.views.ResultView;
 public class ControllerTest {
 
     @Test
-    @DisplayName("Test receiving height and weight")
-    void testGetUserData() {
-        ResultView spyResultView = spy(ResultView.class);
-
-        Float height = 1.70f;
-        Float weight = 70.0f;
-        Controller controller = new Controller(spyResultView);
-
-        controller.requestIMC(height, weight);
-
-        assertThat(height, is(1.70f));
-        assertThat(weight, is(70.0f));
-    }
-    @Test
     @DisplayName("Test requestIMC method")
     void testRequestIMC() {
         ResultView spyResultView = spy(ResultView.class);
