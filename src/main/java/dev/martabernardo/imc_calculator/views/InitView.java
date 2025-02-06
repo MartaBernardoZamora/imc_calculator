1b054787920ce4e9fceef9f65e3de1361f227572
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-02-06 20:10:26 +0100

    Status ok Test initViewPrint method

--- src/main/java/dev/martabernardo/imc_calculator/views/InitView.java
+++ src/main/java/dev/martabernardo/imc_calculator/views/InitView.java
@@ -6,12 +6,18 @@ import java.util.Scanner;
 
 public class InitView {
 
-    public InitView(Scanner mockScanner) {
-        //TODO Auto-generated constructor stub
+    private Scanner scanner;
+    
+    public InitView(Scanner scanner) {
+        this.scanner = scanner;
     }
 
     public void printInitMessage() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'printInitMessage'");
+        System.out.println("Bienvenido a la calculadora de IMC");        
+        System.out.println("Ingrese la altura en metros:");
+        float altura = scanner.nextFloat();        
+        System.out.println("Ingrese el peso en kg:");
+        float peso = scanner.nextFloat(); 
+        System.out.println("Ha ingresado una algura de " + altura + " metros y un peso de " + peso + " kg");
     }
 }
