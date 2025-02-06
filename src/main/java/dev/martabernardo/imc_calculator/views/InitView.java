e950f03f85ad2496b392f232803f166ee305b946
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-02-06 18:39:52 +0100

    Mockito latest stable version dependency and import

--- src/main/java/dev/martabernardo/imc_calculator/views/InitView.java
+++ src/main/java/dev/martabernardo/imc_calculator/views/InitView.java
@@ -1,5 +1,17 @@
 package dev.martabernardo.imc_calculator.views;
 
+import java.util.Scanner;
+
+    
+
 public class InitView {
 
+    public InitView(Scanner mockScanner) {
+        //TODO Auto-generated constructor stub
+    }
+
+    public void printInitMessage() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'printInitMessage'");
+    }
 }
--- src/test/java/dev/martabernardo/imc_calculator/views/InitViewTest.java
+++ src/test/java/dev/martabernardo/imc_calculator/views/InitViewTest.java
@@ -1,6 +1,29 @@
 package dev.martabernardo.imc_calculator.views;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+import java.util.Scanner;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 public class InitViewTest {
 
+    @Test
+    @DisplayName("Test print welcome message")
+    void testInitViewPrint() {
+        Scanner mockScanner = mock(Scanner.class);
+        when(mockScanner.nextFloat()).thenReturn((float)1.75, (float)70.5);
+        
+        InitView initView = new InitView(mockScanner);
+        initView.printInitMessage();
+
+        Mockito.verify(mockScanner, Mockito.times(2)).nextFloat();
+
+
+    }
+    
 }
