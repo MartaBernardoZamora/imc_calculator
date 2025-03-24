43350b20046b7d90fd00a642ee5e919ddb4bfaad
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-24 17:34:44 +0100

    REFACTORY to testGetWeightWithComma from Person model status ok: change testGetWeigth to string

--- src/test/java/dev/martabernardo/models/PersonTest.java
+++ src/test/java/dev/martabernardo/models/PersonTest.java
@@ -3,19 +3,17 @@ package dev.martabernardo.models;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PersonTest {
-
     @Test
     @DisplayName("Test getWeight method")
     void testGetWeight() {
-        Double weight = 70.5;
+        String weight = "70.5";
         Person person = new Person(weight);
 
-        assertThat(person.getWeight(), is(weight));
+        assertThat(person.getWeight(), is(70.5));
     }
     @Test
     @DisplayName("Test getWeight method with ,")
