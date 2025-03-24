76dce304dbcb01984af1e4b13a257f3e3a831211
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-24 17:28:34 +0100

    FAILED testGetWeightWithComma from Person model

--- src/test/java/dev/martabernardo/models/PersonTest.java
+++ src/test/java/dev/martabernardo/models/PersonTest.java
@@ -3,10 +3,12 @@ package dev.martabernardo.models;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PersonTest {
+
     @Test
     @DisplayName("Test getWeight method")
     void testGetWeight() {
@@ -14,5 +16,13 @@ public class PersonTest {
         Person person = new Person(weight);
 
         assertThat(person.getWeight(), is(weight));
+    }
+    @Test
+    @DisplayName("Test getWeight method with ,")
+    void testGetWeightWithComma() {
+        String weight = "70,5";
+        Person person = new Person(weight);
+
+        assertThat(person.getWeight(), is(70.5));
     }
 }
