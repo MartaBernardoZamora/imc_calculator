fefa020f662e25bdfd72d28e6cc9b4fd9e73a1f8
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-24 17:52:40 +0100

    STATUS OK  testGetHeight from Person model

--- src/main/java/dev/martabernardo/models/Person.java
+++ src/main/java/dev/martabernardo/models/Person.java
@@ -3,17 +3,23 @@ package dev.martabernardo.models;
 public class Person {
 
     private Double weight;
+    private Double height;
 
-    public Person(String weight) {
-        this.weight = weightConverter(weight);
+    public Person(String weight, String height) {
+        this.weight = stringToDouble(weight);
+        this.height = stringToDouble(height);
     }
 
     public Double getWeight() {
         return weight;
     }
 
-    private Double weightConverter(String weight) {
-        return Double.parseDouble(weight.replace(",", "."));
+    private Double stringToDouble(String input) {
+        return Double.parseDouble(input.replace(",", "."));
+    }
+
+    public Double getHeight() {
+        return height;
     }
 
 }
