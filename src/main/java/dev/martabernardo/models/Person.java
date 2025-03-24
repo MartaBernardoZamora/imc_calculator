a6ba6735005592933d508e7f7737beb975c76301
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-24 17:24:42 +0100

    STATUS OK testGetWeight from Person model with class, field, constructor and getter

---
+++ src/main/java/dev/martabernardo/models/Person.java
@@ -0,0 +1,15 @@
+package dev.martabernardo.models;
+
+public class Person {
+
+    private Double weight;
+
+    public Person(Double weight) {
+        this.weight = weight;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+}
