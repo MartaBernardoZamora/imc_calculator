87442b7112a1fbdd6bc6182ba5d72719af45852a
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-25 18:35:02 +0100

    FAILED testestRequestIMC from PersonController

---
+++ src/test/java/dev/martabernardo/controller/PersonControllerTest.java
@@ -0,0 +1,28 @@
+package dev.martabernardo.controller;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import dev.martabernardo.views.CalculatorView;
+
+public class PersonControllerTest {
+    @Test
+    @DisplayName("Test requestIMC method")
+    void testRequestIMC() {
+        
+        CalculatorView calculatorView = mock(CalculatorView.class);
+        PersonController personController = new PersonController(calculatorView);
+
+        String height = "1.70";
+        String weight = "70,50";
+
+        personController.requestIMC(height, weight);
+
+        verify(calculatorView, times(1)).printResultMessage(24.39);
+    }
+
+}
