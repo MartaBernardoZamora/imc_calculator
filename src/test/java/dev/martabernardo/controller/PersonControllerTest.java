1708ab2d363e9da4e22765b5f40e4a504cad8f9b
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-25 19:00:10 +0100

    REFACTORY testestRequestIMCWithCategory from PersonController. Delet test with wrong contructor

--- src/test/java/dev/martabernardo/controller/PersonControllerTest.java
+++ src/test/java/dev/martabernardo/controller/PersonControllerTest.java
@@ -11,20 +11,6 @@ import dev.martabernardo.views.CalculatorView;
 
 public class PersonControllerTest {
     @Test
-    @DisplayName("Test requestIMC method")
-    void testRequestIMC() {
-        
-        CalculatorView calculatorView = mock(CalculatorView.class);
-        PersonController personController = new PersonController(calculatorView);
-
-        String height = "1.70";
-        String weight = "70,50";
-
-        personController.requestIMC(height, weight);
-
-        verify(calculatorView, times(1)).printResultMessage(24.39);
-    }
-    @Test
     @DisplayName("Test requestIMC method whith category")
     void testRequestIMCWithCategory() {
         
