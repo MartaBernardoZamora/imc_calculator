d8f8cf53447b8e06ad32fd4cc6618d5eba5b8ff1
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-25 20:19:52 +0100

    FAILED testAppRunning

--- src/test/java/dev/martabernardo/AppTest.java
+++ src/test/java/dev/martabernardo/AppTest.java
@@ -1,12 +1,18 @@
 package dev.martabernardo;
 
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+
 import org.junit.jupiter.api.DisplayName;
 
 class AppTest {
-    @Test 
-    @DisplayName("Test app functionality") 
-    void testMain() { 
-        App.main(new String[]{}); 
+    @Test
+    @DisplayName("Test if the initMessage method is called")
+    void testAppRunning() {
+        String input = "1.70\n70.50\n";
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        App.main(new String[]{});
     }
 }
