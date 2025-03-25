958255d3585abdec105a74957776540da04b06e5
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-25 19:48:52 +0100

    FAILED testPrintResultMessage from CalculatorView

--- src/test/java/dev/martabernardo/views/CalculatorViewTest.java
+++ src/test/java/dev/martabernardo/views/CalculatorViewTest.java
@@ -4,8 +4,11 @@ import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 import dev.martabernardo.controller.PersonController;
 
@@ -25,5 +28,22 @@ public class CalculatorViewTest {
  
         calculatorView.initMessage();
         verify(controller).requestIMC("1.70", "70.50");
+    }
+    @Test
+    @DisplayName("Test printResultMessage method")
+    void testPrintResultMessage() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        Scanner scanner = mock(Scanner.class);
+        PersonController controller = mock(PersonController.class);
+        CalculatorView calculatorView = new CalculatorView(scanner, controller);
+
+        calculatorView.printResultMessage(24.39, "peso normal");
+
+        String output = outContent.toString();
+        assertTrue(output.contains("24.39"));
+        assertTrue(output.contains("Usted presenta: peso normal"));
+
     }
 }
